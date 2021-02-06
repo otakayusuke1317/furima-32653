a598fd7b264fec71750af5499e85a59984041090
otakeyusuke1317 <big_high_enperor@yahoo.co.jp>
2021-02-06 20:57:11 +0900

    単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-   validates :nickname,           presence: true
-   validates :email   ,           presence: true
-   validates :encrypted_password, presence: true
-   validates :family_name,        presence: true
-   validates :first_name,         presence: true
-   validates :family_name_kana,   presence: true
-   validates :first_name_kana,    presence: true
-   validates :birth_day,          presence: true
+
+  with_options presence: true do
+   validates :nickname,           length: { maximum: 20 }
+   validates :family_name,        format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "is invalid. Input full-width characters." }
+   validates :first_name,         format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "is invalid. Input full-width characters." }
+   validates :family_name_kana,   format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters." }
+   validates :first_name_kana,    format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters." }
+   validates :birth_day
+   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+  end
 end
 
