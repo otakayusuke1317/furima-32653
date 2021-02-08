7d9cdc42b0ebf10aa9d9d7d515b58423142b0e89
otakeyusuke1317 <big_high_enperor@yahoo.co.jp>
2021-02-08 12:34:23 +0900

    モデル＿テーブル作成

---
+++ app/models/item.rb
@@ -0,0 +1,14 @@
+class Item < ApplicationRecord
+ 
+  with_options presence: true do
+    validates :name
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+    validates :description
+    validates :status_id
+    validates :shipping_cost_id
+    validates :shipping_day_id
+    validates :prefecture_id
+    validates :category_id
+    validates :use_id　
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_03_081312) do
+ActiveRecord::Schema.define(version: 2021_02_08_020025) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", default: "", null: false
+    t.integer "price", null: false
+    t.text "description", null: false
+    t.integer "status_id", null: false
+    t.integer "shipping_cost_id", null: false
+    t.integer "shipping_day_id", null: false
+    t.integer "prefecture_id", null: false
+    t.integer "category_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", default: "", null: false
