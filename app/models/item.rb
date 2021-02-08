31d0acb6b7290d2314f58e6d6936a35b23e68ea3
otakeyusuke1317 <big_high_enperor@yahoo.co.jp>
2021-02-08 20:52:05 +0900

    バリデーションを設定する

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,22 +1,25 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   
-  has_one_attached :image
-  belongs_to :status
-  belongs_to :category
-  belongs_to :shipping_cost
-  belongs_to :shipping_day
-  belongs_to :prefecture
+   has_one_attached :image
+   belongs_to :status
+   belongs_to :category
+   belongs_to :shipping_cost
+   belongs_to :shipping_day
+   belongs_to :prefecture
 
   with_options presence: true do
     validates :name
     validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+    validates :price, numericality: { only_integer: true, message: "Half-width number." }
     validates :description
-    validates :status_id
-    validates :shipping_cost_id
-    validates :shipping_day_id
-    validates :prefecture_id
-    validates :category_id
-    validates :use_id　
+    validates :status_id, :shipping_cost_id, :shipping_day_id, :prefecture_id, :category_id 
+    validates :use_id
+    validates :image
+  
+    #空の投稿を保存できないようにする
+    validates :name, :description, presence: true
+    #選択が「--」の時は保存できないようにする
+    validates :status_id, :shipping_cost_id, :shipping_day_id, :prefecture_id, :category_id, numericality: { other_than: 1 } 
   end
 end
