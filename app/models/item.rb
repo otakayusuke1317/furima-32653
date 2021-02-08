5543d1bf0c963378eafa03178699943e0a955d03
otakeyusuke1317 <big_high_enperor@yahoo.co.jp>
2021-02-08 14:38:55 +0900

    ActiveStorageを導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,7 @@
 class Item < ApplicationRecord
  
+  has_one_attached :image
+
   with_options presence: true do
     validates :name
     validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
