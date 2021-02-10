14a6c1a857a9cef92e734054e110b31b591c705c
otakeyusuke1317 <big_high_enperor@yahoo.co.jp>
2021-02-10 15:06:13 +0900

    イベント発火

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -2,5 +2,9 @@ window.addEventListener('load', () => {
   console.log("OK");
   const priceInput = document.getElementById("item-price");
   console.log(priceInput);
+  priceInput.addEventListener("input", () => {
+    console.log("イベント発火");
+ }) 
 });
+  
 
