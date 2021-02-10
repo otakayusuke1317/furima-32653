5727d1a3af05d32b795c8b445a75d19706f0fc59
otakeyusuke1317 <big_high_enperor@yahoo.co.jp>
2021-02-10 15:26:00 +0900

    value属性

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -3,8 +3,7 @@ window.addEventListener('load', () => {
   const priceInput = document.getElementById("item-price");
   console.log(priceInput);
   priceInput.addEventListener("input", () => {
-    console.log("イベント発火");
- }) 
+    const inputValue = priceInput.value;
+    console.log(inputValue);
+  }) 
 });
-  
-
