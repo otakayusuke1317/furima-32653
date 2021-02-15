4f23c5253ba740b066356aa5439aacc5bd8adcc2
otakeyusuke1317 <big_high_enperor@yahoo.co.jp>
2021-02-15 16:41:26 +0900

    エラーが表示される

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-
+  
+  has_one :purchase_record, dependent: :destroy
   has_one_attached :image
   belongs_to :status
   belongs_to :category
