700723e2a99be7473e9335c65dfca4e943ab5c86
otakeyusuke1317 <big_high_enperor@yahoo.co.jp>
2021-02-26 20:24:54 +0900

    ransackインストール

---
+++ spec/requests/products_request_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe "Products", type: :request do
+
+  describe "GET /item" do
+    it "returns http success" do
+      get "/products/item"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /search" do
+    it "returns http success" do
+      get "/products/search"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
